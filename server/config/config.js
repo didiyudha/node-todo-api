2224fabbcf0ee3fee6569bafb0ddc2eb87e9f7c9
Didi Yudha Perwira <didiyudha@gmail.com>
2017-07-12 15:51:08 +0700

    Storing configuration into json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,14 @@
 var env = process.env.NODE_ENV || 'development';
 
-console.log('environment: ', env);
-
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// Configuration of environment
+// Configuration stored in config.json file in this directory
+// There are two configuration: development and test
+// For production we use environment of Heroku
+// Load config.json file retrieve all the keys and values and set them into environment process
+if (env === 'development' || env === 'test') {
+    var config = require('./config.json');
+    var envConfig = config[env];
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
 }
